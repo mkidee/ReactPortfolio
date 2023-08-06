c91a2cf15471bdd3f9d0e545e6f6aeab38f6deaa
mkidee <mckaymemmott@gmail.com>
2023-08-05 18:20:49 -0600

    set up on main jsx page and some on main app

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,12 @@
 import React, { useState } from 'react';
+import about from './about';
 import contact from './contact';
 import portfolio from './portfolio';
 import resume from './resume';
+
+
+export default function Pages() {
+    const [page, setPage] = useState('about');
+
+    const pageContentHandler = (page) => setPage(page);
+}
