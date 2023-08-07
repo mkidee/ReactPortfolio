77f1174c5e9658eea14006746d036a0b32c81dfd
mkidee <mckaymemmott@gmail.com>
2023-08-06 22:31:39 -0600

    footer and header classes done with links and exported

--- src/components/NavHeadFoot.jsx
+++ src/components/NavHeadFoot.jsx
@@ -11,22 +11,50 @@ function TopNav({ page, pageContentHandler }) {
     return (
         <ul className={'tabs'}>
 
-            <li className='tab-links'>
+            <li className='tabLinks'>
                 <a href="#about" onClick={() => pageContentHandler('About Me')} className={linkState('About Me')}>About Me</a>
             </li>
 
-            <li className='tab-links'>
+            <li className='tabLinks'>
                 <a href='#portfolio' onClick={() => pageContentHandler('Portfolio')} className={linkState('Portfolio')}>My Portfolio</a>
             </li>
 
-            <li className='tab-links'>
+            <li className='tabLinks'>
                 <a href='#resume' onClick={() => pageContentHandler('Resume')} className={linkState('Resume')}>My Resume</a>
             </li>
 
-            <li className='tab-links'>
+            <li className='tabLinks'>
                 <a href='#contact' onClick={() => pageContentHandler('Contact')} className={linkState('Contact')}>Contact Me</a>
             </li>
 
         </ul>
     );
 };
+
+function Header() {
+    return (
+        <h1>Mckay Memmott</h1>
+    );
+};
+
+function Footer() {
+    return (
+        <section className='footerSpace'>
+
+            <div className='footerLink'>
+                <a href='https://github.com/mkidee' target='_blank' rel='noopener noreferrer'>
+                    <img src={githubicon} alt='github logo link' width='50' height='50' />
+                </a>
+            </div>
+
+            <div className='footerLink'>
+                <a href='https://www.linkedin.com/in/mckay-memmott-48a166263/' target='_blank' rel='noopener noreferrer'>
+                    <img src={linkedicon} alt='linkedIn logo link' width='50' height='50' />
+                </a>
+            </div>
+
+        </section>
+    );
+};
+
+export { TopNav, Header, Footer };
